43d8854d038efe2f09c6a5206a8b94681f78452b
Guiltyyy-cmd <hypoflixdormnet@gmail.com>
2026-07-28 12:10:45 +0530

    Create __init__.py

---
+++ handlers/__init__.py
@@ -0,0 +1,24 @@
+"""
+==========================================
+ONICORE BOTS
+
+Bot Name : Jhaplupirobot
+Developer : @BlurpleOg
+
+Marketing :
+#onicore_bots
+
+==========================================
+"""
+
+# Import handlers here.
+
+from .start import *
+
+# Future Imports
+
+# from .settings import *
+# from .admin import *
+# from .status import *
+# from .broadcast import *
+# from .callback import *
